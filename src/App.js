267ce6108afe712d1a40a216bb01d25cbc5f7e59
Alejandro <alejobomu@gmail.com>
2025-10-16 17:07:11 -0500

    Primer commit: estructura base de React creada

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenido a GameTracker</h1>
+      <p>Tu biblioteca personal de videojuegos comienza aquí.</p>
+    </div>
+  );
+}
+
+export default App;
